f306831f32db734fe2844d03f02e3b3488943fd6
Shermaily Lee <150724982+Shermaily@users.noreply.github.com>
2023-12-04 13:48:12 -0500

    Update SCRIPT.js

--- SCRIPT.js
+++ SCRIPT.js
@@ -1,12 +1,12 @@
-let iconos = [] // Declara una matriz vacía para almacenar los iconos
-let selecciones = [] // Declara una matriz vacía para almacenar las tarjetas seleccionadas
+let iconos = [] 
+let selecciones = [] 
 
+generarTablero() 
 
-generarTablero() // Llama a la función para generar el tablero de juego
 
 function cargarIconos() {
   iconos = [
-    '<i class="fas fa-star"></i>', //Adición de iconos a la matriz
+    '<i class="fas fa-star"></i>', 
     '<i class="far fa-star"></i>',
     '<i class="fas fa-star-of-life"></i>',
     '<i class="fas fa-star-and-crescent"></i>',
@@ -21,11 +21,11 @@ function cargarIconos() {
   ]
 }
 
-function generarTablero() { //Cargue los iconos antes de generar el tablero
+function generarTablero() { 
   cargarIconos()
-  selecciones = [] // Borrar la matriz de tarjetas seleccionadas
+  selecciones = [] 
   let tablero = document.getElementById("tablero")//Obtener el elemento que representa el tablero de juego
-  let tarjetas = [] // Crear un array vacío para almacenar el HTML de la tarjeta
+  let tarjetas = []
   for (let i = 0; i < 24; i++) {  // Recorre 24 cartas
     tarjetas.push(`
       <div class="area-tarjeta" onclick="seleccionarTarjeta(${i})">
@@ -55,9 +55,8 @@ function seleccionarTarjeta(i) {
     // Si la tarjeta no está girada, se gira y se agrega su índice al arreglo 'selecciones'.
     tarjeta.style.transform = "rotateY(180deg)"
     selecciones.push(i)
-  }// Se verifica si hay dos tarjetas seleccionadas.
+  }
   if (selecciones.length == 2) {
-    // Si hay dos tarjetas seleccionadas, se llama a la función 'deseleccionar' y se reinicia el arreglo 'selecciones'.
     deseleccionar(selecciones)
     selecciones = []
   }
@@ -65,22 +64,21 @@ function seleccionarTarjeta(i) {
 
 function deseleccionar(selecciones) {
   setTimeout(() => { // Se utiliza setTimeout para introducir un retardo en la ejecución del siguiente bloque de código.
-    let trasera1 = document.getElementById("trasera" + selecciones[0]) // Se obtiene el elemento de la parte trasera de la tarjeta correspondiente al primer elemento seleccionado.
+    let trasera1 = document.getElementById("trasera" + selecciones[0]) 
     let trasera2 = document.getElementById("trasera" + selecciones[1])// Se obtiene el elemento de la parte trasera de la tarjeta correspondiente al segundo elemento seleccionado.
     if (trasera1.innerHTML != trasera2.innerHTML) { // Se verifica si el contenido de las tarjetas seleccionadas no es el mismo.
        let tarjeta1 = document.getElementById("tarjeta" + selecciones[0])
       let tarjeta2 = document.getElementById("tarjeta" + selecciones[1])
       // Si el contenido no es el mismo, se obtienen los elementos de las tarjetas correspondientes al primer y segundo elementos seleccionados.
      
-      // Se ajusta la propiedad de transformación  de las tarjetas para ocultarlas.
       tarjeta1.style.transform = "rotateY(0deg)"
       tarjeta2.style.transform = "rotateY(0deg)"
     }else{
                  
-                    trasera1.style.background = "plum" // Cambia el color del fondo para indicar la coindencia
+                    trasera1.style.background = "plum" 
                     trasera2.style.background = "plum"
                 }
-            }, 1000); // Retrasar el cambio de color de fondo para permitir la animación de voltear la carta
+            }, 1000); 
         }
 
 
